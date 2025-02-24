d5e8558476dc2ac72c26d4e7489ce814794637ae
Fabian Thomys <git@fthomys.me>
2025-02-24 11:29:07 +0100

    Integrate TailwindCSS and update Nuxt application structure
    - Added TailwindCSS setup with `tailwind.config.js` and `app.css`.
    - Updated `app.vue` to use `NuxtLayout` and `NuxtPage` for better layout handling.
    - Configured `nuxt.config.ts` to include TailwindCSS and PostCSS plugins.
    - Modified `compose.yml` to use `.env` for database and MinIO configuration.
    - Updated `package-lock.json` with new dependencies for TailwindCSS and DaisyUI.
    - Updated `docker-compose` volumes to use temporary directories (for
      development).

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import path from "node:path";
+
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  css: ['@/assets/app.css'],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
 })
+
